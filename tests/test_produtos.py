be035b0a28a14b7ca31aae44768bc372db5d17bd
tiagomoro76-ops <tiagomoro76@gmail.com>
2026-04-25 15:12:09 -0300

    Adicionar 5 testes unitários

---
+++ tests/test_produtos.py
@@ -0,0 +1,25 @@
+from produtos import produtos
+
+
+def test_lista_nao_vazia():
+    assert len(produtos) > 0
+
+
+def test_existe_categoria_eletronicos():
+    categorias = [p["categoria"] for p in produtos]
+    assert "Eletrônicos" in categorias
+
+
+def test_todos_produtos_tem_nome():
+    for produto in produtos:
+        assert "nome" in produto
+
+
+def test_todos_produtos_tem_preco():
+    for produto in produtos:
+        assert "preco" in produto
+
+
+def test_precos_maiores_que_zero():
+    for produto in produtos:
+        assert produto["preco"] > 0
